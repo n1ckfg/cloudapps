7f62f804336c3ce99e90650b4ee16363a4ebe074
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-07-08 12:15:52 -0400

    disable message logging

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const wss = new WebSocket.Server({ port: PORT });
  
 wss.on('connection', function connection(ws, req) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
     wss.clients.forEach(function(client) {       
         if (client !== ws) {
             try {
