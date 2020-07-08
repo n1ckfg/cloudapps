ad302ab22aef357111c824f8fe98022200dfac77
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-07-08 16:42:29 -0400

    install scripts, also updated gotlib

---
+++ postinstall.js
@@ -0,0 +1,8 @@
+const {exec} = require('child_process')
+
+// update the submodule(s) in this repo
+// please don't remove this file!
+
+exec('git submodule update --remote', (stdout,stderr,err) =>{
+    console.log(stdout)
+})
