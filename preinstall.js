cc2af6af7eb4fc1da9d5f5c95c61715f0ffc0ed1
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-07-11 12:56:14 -0400

    added submodule update to preinstall

--- preinstall.js
+++ preinstall.js
@@ -2,10 +2,11 @@ const {exec} = require('child_process')
 const fs = require('fs')
 // update the submodule(s) in this repo
 // please don't remove this file!
-exec('git config submodule.recurse true')
 if (fs.existsSync(__dirname + '/gotlib')) {
     // Do something
-    console.log(__dirname + '/gotlib already installed')
+    console.log(__dirname + '/gotlib already installed, updating')
+    exec('git submodule update --init --recursive', (stdout,stderr,err) =>{
+    })
 } else {
     exec('git submodule add -b master https://github.com/worldmaking/gotlib.git', (stdout,stderr,err) =>{
     console.log(stdout, stderr, err)
