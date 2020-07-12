237d54bc79d09490f941d27a863c30ac01ae4e61
Zodsmar <zodsmar@gmail.com>
2020-07-12 11:54:36 -0400

    this was supposed to be a fun addition, not a major diversion

--- host.js
+++ host.js
@@ -271,7 +271,7 @@ let recordStatus = 0
 
       case "nuclearOption":
         // close all this client's connection
-        let strangelove = strangelove[strangelove.length * Math.random() | 0]
+        strangelove = strangelove[strangelove.length * Math.random() | 0]
         msg = JSON.stringify({
           cmd:'nuclearOption',
           date: Date.now(),
