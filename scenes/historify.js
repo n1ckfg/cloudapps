460eb0561a64993e631691ef14eede34de00ce1c
Zodsmar <zodsmar@gmail.com>
2020-07-20 14:05:33 -0400

    set history value to 'no'

--- scenes/historify.js
+++ scenes/historify.js
@@ -1,25 +1,40 @@
 const fs = require('fs')
+let sceneFiles = ['scene_complicated.json',       
+'scene_cycle.json',
+'scene_edited.json',
+'scene_feedback.json',
+'scene_noise.json',
+'scene_rich.json',
+'self_patched.json']
 
-let scene = JSON.parse(fs.readFileSync('simple.json'))
+
+    update(sceneFiles[7])
 
 
-let items = Object.keys(scene.nodes)
+function update(file){
+
+    let scene = JSON.parse(fs.readFileSync('simple.json'))
 
 
-for(i=0;i<items.length;i++){
-    let module = scene.nodes[items[i]]
-    let nodes = Object.keys(module)
-    // console.log(module,nodes)
-    for(j=0;j<nodes.length;j++){
-        if(module[nodes[j]]._props && module[nodes[j]]._props.kind === 'outlet'){
+    let items = Object.keys(scene.nodes)
+
+
+    for(i=0;i<items.length;i++){
+        let module = scene.nodes[items[i]]
+        let nodes = Object.keys(module)
+        // console.log(module,nodes)
+        for(j=0;j<nodes.length;j++){
+            if(module[nodes[j]]._props && module[nodes[j]]._props.kind === 'outlet'){
+                
+                module[nodes[j]]._props['history'] = 'no'
+                console.log(module)
+                
+            }
             
-            module[nodes[j]]._props['history'] = false
-            console.log(module)
-            
+
         }
         
-
     }
-    
+    fs.writeFileSync('simple.json', JSON.stringify(scene, null, 2))
+
 }
-fs.writeFileSync('simple.json', JSON.stringify(scene, null, 2))
