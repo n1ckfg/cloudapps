88a7669d5bcab3e94341eab7542c907ece3e4310
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-08-19 18:40:53 -0400

    remove pre and post install scripts

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-const {exec} = require('child_process')
-
-// update the submodule(s) in this repo
-// please don't remove this file!
-
-// exec('git submodule update --remote', (stdout,stderr,err) =>{
-//     console.log(stdout)
-// })
